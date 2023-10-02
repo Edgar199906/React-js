467aec61c39abcfcf8e3e5a4dd54cb896db07134
Edgar Ohanyan <edgar1999vzgo@mail.ru>
2023-10-02 20:45:06 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,96 @@
+"use strict";
+
+// function greet() {
+//     console.log("Hello World");
+// }
+// greet();
+
+// const sevan = 7;
+
+// function squaring(){
+//     console.log(sevan);
+//     return sevan * sevan;
+// }
+// squaring();
+// let count = 0;
+// function incrase(){
+//     count = ++count;
+
+// }
+// incrase()
+// console.log(count)
+// incrase()
+// incrase()
+// incrase()
+// console.log(count)
+
+// let country = "Armenia";
+// function print_country_name(){
+//     let country = "Russia ";
+//     console.log(country)
+// }
+// print_country_name()
+
+// function multiplay (x, y){
+//     return x * y;
+// }
+// const six = multiplay (3, 2);
+// console.log(six);
+// function divide(){
+//     console.log(y)
+//     return x * y ;
+// }
+// divide(4, 4)
+// let person = {
+//     firstName: "Aren",
+//     lastName:  "Arenyan"
+// }
+
+// let newPerson = Object.assign(person)
+
+// console.log(newPerson)
+
+// const numbers = [1, 10   , 3, 15, 30, 8, 66];
+
+// let  arr = ["8", "aaa", "working"];
+
+// arr.splice
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     let arr = [1,2,3,4,5];
+
+//     const result = arr.reduce((sum, item)=> sum + item, 0);
+
+//     console.log(result);
+// }
+
+// let map = new Map();
+
+// map.set("1", "str1");
+// map.set(1, "str1");
+// map.set("1", "str1");
+
+// console.log(map.get(1));
+
+// let Arman = {
+//      name: "Arman"
+// }
+// map.set(Arman, )
+
+// function num(a) {
+//   try {
+//     if (a < 10) {
+//       console.log("Ok");
+//     } else {
+//       throw new Error("This is a wring");
+//     }
+//   } catch (error) {
+//     console.log("Error: " + error);
+//   } finally {
+//     console.log("finely");
+//   }
+// }
+// num(15);
+
+
